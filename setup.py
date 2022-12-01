6ceb5b4bc4a829e9ae6b7e79de315656b17abd6e
Jagruti <jagrutigodambe1234@gmail.com>
2022-12-01 16:54:38 +0100

    color added

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+setup(name="let_it_snow",
+      version="0.1",
+      author="jagruti",
+      packages=find_packages(),
+      install_package=["numpy",'turtle']
+)
